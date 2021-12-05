b041b9b22284c08b15d7bd9619cd0902f62ba65b
Spandan Saxena <spandansaxena@gmail.com>
2021-12-05 14:57:00 +0530

    Added location permission requests

--- app/src/main/java/com/onesilicondiode/handyheater/MainActivity.kt
+++ app/src/main/java/com/onesilicondiode/handyheater/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.onesilicondiode.handyheater
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,11 +14,8 @@ import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.PowerManager
 import android.view.WindowManager
-
-
-
-
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         freeze.visibility = View.GONE
 
-
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -35,7 +33,23 @@ class MainActivity : AppCompatActivity() {
         freeze.visibility = View.VISIBLE
         heat.visibility = View.GONE
         acquireWakeLock()
+        startGPS();
         haptics()
+    }
+
+    private fun startGPS() {
+        if (!checkPermission()) {
+            requestPermission()
+        }
+    }
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION)
+        val result1 = ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_COARSE_LOCATION)
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 1)
     }
 
     private fun acquireWakeLock() {
