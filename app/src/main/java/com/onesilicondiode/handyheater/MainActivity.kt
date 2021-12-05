f446d1ebd9ce4b68927dc33e94276e97af1fac64
shankar <shankarpriyank@gmail.com>
2021-12-05 14:23:04 +0530

    try!!!!

--- app/src/main/java/com/onesilicondiode/handyheater/MainActivity.kt
+++ app/src/main/java/com/onesilicondiode/handyheater/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.onesilicondiode.handyheater
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun StartHeating(view: View) {
+        freeze.setTransitionVisibility(View.VISIBLE)
+
+
+        heat.setTransitionVisibility(View.INVISIBLE)
+
+    }
+    fun stopHeating(view: View) {}
 }
