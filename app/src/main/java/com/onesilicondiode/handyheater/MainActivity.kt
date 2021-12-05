ffd44f2197d5ee0071042ddd6b5a78d588081769
Spandan Saxena <spandansaxena@gmail.com>
2021-12-05 14:51:46 +0530

    Wake Lock Added

--- app/src/main/java/com/onesilicondiode/handyheater/MainActivity.kt
+++ app/src/main/java/com/onesilicondiode/handyheater/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.onesilicondiode.handyheater
 
+import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,8 +9,15 @@ import android.os.Vibrator
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
+import android.os.PowerManager
+import android.view.WindowManager
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -27,8 +35,17 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Heating Begins", Toast.LENGTH_SHORT).show()
         freeze.visibility = View.VISIBLE
         heat.visibility = View.GONE
+        acquireWakeLock()
         haptics()
     }
+
+    private fun acquireWakeLock() {
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+                    or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+        )
+    }
+
     fun stopHeating(view: View) {
         Toast.makeText(this, "Heating Stop", Toast.LENGTH_SHORT).show()
         freeze.visibility = View.GONE
